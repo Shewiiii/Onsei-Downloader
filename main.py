5cb97d8370baf7c8eb9f7f5265719cc24ec09a2e
Shewiiii <akiempj123@gmail.com>
2024-07-19 15:37:59 +0200

    Rename variables

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ def get_onsei_api(
 
 def tag_file(
     file_path: str | Path,
-    cover: bytes,
+    cover_image: bytes,
     title: str,
     work_title: str
 ) -> None:
@@ -50,7 +50,7 @@ def tag_file(
     f['title'] = title
     f['album'] = work_title
     f['tracknumber'] = onep(title)
-    f['artwork'] = cover
+    f['artwork'] = cover_image
     f.save()
     logging.info(f'Tagged file: {file_path}')
 
@@ -89,15 +89,15 @@ def save_text_file(file_path: Path, media_stream_url: str) -> None:
         file.write(requests.get(media_stream_url).text)
 
 
-def save_cover_image(cover_path: Path, cover: bytes) -> None:
-    cover_path.write_bytes(cover)
+def save_cover_image(cover_path: Path, cover_image: bytes) -> None:
+    cover_path.write_bytes(cover_image)
     logging.info(f'Saved cover: {cover_path}')
 
 
 def process_file(
     onsei_api: dict,
     path: Path,
-    cover: bytes,
+    cover_image: bytes,
     ignore: list
 ) -> None:
     file_type = onsei_api['type']
@@ -116,18 +116,18 @@ def process_file(
         if file_downloaded and file_type == 'audio':
             tag_file(
                 file_path=file_path,
-                cover=cover,
+                cover_image=cover_image,
                 title=os.path.splitext(onsei_api['title'])[0],
                 work_title=onsei_api['workTitle']
             )
         cover_path = path / 'cover.jpg'
         if not cover_path.is_file():
-            save_cover_image(cover_path, cover)
+            save_cover_image(cover_path, cover_image)
 
 
 def recursive_download(
     onsei_api: list | dict,
-    cover: bytes,
+    cover_image: bytes,
     path: Path = Path('.'),
     ignore: list[str] = []
 ) -> None:
@@ -137,7 +137,12 @@ def recursive_download(
     # Folder/file list at a certain folder depth
     if isinstance(onsei_api, list):
         for element in onsei_api:
-            recursive_download(element, cover, path, ignore)
+            recursive_download(
+                element,
+                cover_image,
+                path,
+                ignore
+            )
     # Folder api dict
     elif onsei_api['type'] == 'folder':
         folder_name: str = onsei_api['title']
@@ -146,10 +151,15 @@ def recursive_download(
             return
         folder_path = path / folder_name
         create_folder(folder_path)
-        recursive_download(onsei_api['children'], cover, folder_path, ignore)
+        recursive_download(
+            onsei_api['children'],
+            cover_image,
+            folder_path,
+            ignore
+        )
     # File api dict
     else:
-        process_file(onsei_api, path, cover, ignore)
+        process_file(onsei_api, path, cover_image, ignore)
 
 
 def read_config(
@@ -170,7 +180,7 @@ def fetch_cover_image(
     return response.content
 
 
-def complete_download(
+def download_onsei(
     code: str
 ) -> None:
     config = read_config('config.json')
@@ -179,16 +189,16 @@ def complete_download(
         logging.error('No work_id found in user input')
         return
     root_path = Path(config['rootPath']) / code
-    ignore_list = config['ignore']
+    ignore: list = config['ignore']
     root_path.mkdir(parents=True, exist_ok=True)
     logging.info(f'Created folder: {root_path}')
     cover_image = fetch_cover_image(work_id)
     onsei_api = get_onsei_api(work_id)
 
-    recursive_download(onsei_api, cover_image, root_path, ignore_list)
+    recursive_download(onsei_api, cover_image, root_path, ignore)
 
 
 if __name__ == '__main__':
     user_input: str = input('RJ/VJ code: ')
     for code in user_input.split(','):
-        complete_download(code)
+        download_onsei(code)
